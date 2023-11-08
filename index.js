c894f7ba60329a41f684d02fdbcc3458d5149073
chegg <chegg.6964@gmail.com>
2023-11-08 13:53:06 +0700

    perubahan index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+async function sendFonnte(data) {
+    const url = "https://api.fonnte.com/send";
+
+    const customHeaders = {
+        "Content-Type": "application/json",
+        Authorization: TOKEN,
+    };
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: customHeaders,
+        body: JSON.stringify(data),
+    });
+    console.log(await response.json());
+}
+
+app.post("/webhook", function (req, res) {
+    console.log(req.body);
+    if (req.body.message == "test") {
+        const data = {
+            target: req.body.sender,
+            message: "working great!",
+        };
+        sendFonnte(data);
+    } else {
+        const data = {
+            target: req.body.sender,
+            message: "this is default reply from fonnte",
+        };
+        sendFonnte(data);
+    }
+    res.end();
+});
+
+app.listen(process.env.PORT || 3000, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", 3000);
+});
+// app.listen(process.env.PORT || 3000)
